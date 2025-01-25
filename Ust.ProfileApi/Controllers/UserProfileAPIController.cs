a35516a15b3478b3269e005b8088a729dda5eb24
boney84 <boney0084@gmail.com>
2025-01-25 08:54:34 +0530

    Add project files.

---
+++ Ust.ProfileApi/Controllers/UserProfileAPIController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ust.ProfileApi.Models;
+
+namespace Ust.ProfileApi.Controllers
+{
+    [Route("api/UserProfile")]
+    [ApiController]
+    public class UserProfileAPIController : ControllerBase
+    {
+        private readonly UstUserProfileContext _context;
+
+        public UserProfileAPIController()
+        {
+            _context = new UstUserProfileContext();
+        }
+
+        // GET: api/UserProfileAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserProfile>>> GetUserProfile()
+        {
+            return await _context.UserProfile.ToListAsync();
+        }
+
+        // GET: api/UserProfileAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserProfile>> GetUserProfile(int id)
+        {
+            var userProfile = await _context.UserProfile.FindAsync(id);
+
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            return userProfile;
+        }
+
+        // PUT: api/UserProfileAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserProfile(int id, UserProfile userProfile)
+        {
+            if (id != userProfile.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userProfile).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserProfileExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserProfileAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserProfile>> PostUserProfile(UserProfile userProfile)
+        {
+            _context.UserProfile.Add(userProfile);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserProfile", new { id = userProfile.Id }, userProfile);
+        }
+
+        // DELETE: api/UserProfileAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserProfile(int id)
+        {
+            var userProfile = await _context.UserProfile.FindAsync(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserProfile.Remove(userProfile);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserProfileExists(int id)
+        {
+            return _context.UserProfile.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Ust.ProfileApi/Models/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ust.ProfileApi.Models
+{
+    public class UserProfile
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone {  get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Description {  get; set; }
+        public  string City {  get; set; }
+        public string Address {  get; set; }
+        public string SkypeId { get; set; }
+    }
+}
